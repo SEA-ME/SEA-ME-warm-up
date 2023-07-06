9bf5ee0da15eb80e93e759231d021f96e49a69c2
oluwasegun oyekeye <118446979+oluwasegun-oyekeye@users.noreply.github.com>
2023-07-06 11:07:02 +0100

    Update convert.cpp

--- Exercise02/convert.cpp
+++ Exercise02/convert.cpp
@@ -1,30 +1,33 @@
 #include <iostream>
 #include <string>
-#include <algorithm>  // Required for std::transform function
+#include <algorithm>  // Required for string transformation
 
-int main(int argc, char* argv[]) {
-  // Check the number of command-line arguments
-  if (argc != 3) {
-    std::cout << "Usage: " << argv[0] << " {command} {string}" << std::endl;
-    return 1;
-  }
+int main() {
+  // Ask the user for a command and a string
+  std::cout << "Enter a command ('up' or 'down'): ";
+  std::string command;
+  std::cin >> command;
+  
+  std::cout << "Enter a string: ";
+  std::string str;
+  std::cin.ignore(); // Ignore the newline character in the input buffer
+  std::getline(std::cin, str);
 
-  std::string command = argv[1];  // Retrieve the command argument
-  std::string str = argv[2];      // Retrieve the string argument
-
-  // Convert the string to uppercase or lowercase based on the command
+  // Convert the string based on the command
   if (command == "up") {
+    // Convert the string to uppercase
     std::transform(str.begin(), str.end(), str.begin(), ::toupper);
   } else if (command == "down") {
+    // Convert the string to lowercase
     std::transform(str.begin(), str.end(), str.begin(), ::tolower);
   } else {
     // Handle the case of an invalid command
     std::cout << "Invalid command. Please use 'up' or 'down'." << std::endl;
-    return 1;
+    return 1; // Exit the program with an error code
   }
 
-  // Output the converted string
-  std::cout << "Output: " << str << std::endl;
+  // Display the converted string
+  std::cout << "Converted string: " << str << std::endl;
 
-  return 0;
+  return 0; // Exit the program successfully
 }
