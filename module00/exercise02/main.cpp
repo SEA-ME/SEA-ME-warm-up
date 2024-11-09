046119fc62b98281630dc6ebf00dc1c3a00da6e9
Rui Rocha <rui.rocha@ctw.bmwgroup.com>
2024-11-09 15:24:59 +0000

    Removed using namespace and added message in case of non implemented option

--- module00/exercise02/main.cpp
+++ module00/exercise02/main.cpp
@@ -2,8 +2,6 @@
 #include <algorithm>
 #include <vector>
 
-using namespace std;
-
 bool validate_command(const std::string&);
 std::string uppercase_string(const std::string&);
 std::string lowercase_string(const std::string&);
@@ -14,19 +12,19 @@ const std::vector<std::string> commands = { "up", "down" };
 int main(int argc, char** argv)
 {
     if(argc < 3) {
-        cout << "Example usage: ./convert {command} {string}" << endl;
+        std::cout << "Example usage: ./convert {command} {string}" << std::endl;
         return 1;
     }
 
-    const string command(argv[1]);
+    const std::string command(argv[1]);
 
     if(!validate_command(command)) {
-        cout << "Invalid command" << endl;
+        std::cout << "Invalid command" << std::endl;
         return 1;
     }
 
     // Copying all arguments into a container of strings
-    vector<string> text(argv + 2, argv + argc);
+    std::vector<std::string> text(argv + 2, argv + argc);
 
     // UP
     if(command == commands[0]) {
@@ -36,16 +34,20 @@ int main(int argc, char** argv)
     else if(command == commands[1]) {
         transform(text.begin(), text.end(), text.begin(), ::lowercase_string);
     }
+    // FUTURE UNIMPLEMENTED COMMANDS
+    else {
+        std::cout << "Operation not implemented\n";
+    }
 
     // Printing the changed text
     for (const auto & i : text) {
-        cout << i << " ";
+        std::cout << i << " ";
     }
-    cout << endl;
+    std::cout << std::endl;
     return 0;
 }
 
-bool validate_command(const string& command) {
+bool validate_command(const std::string& command) {
     if(find(commands.begin(), commands.end(), command) != commands.end()) {
         return true;
     }
@@ -53,14 +55,14 @@ bool validate_command(const string& command) {
     return false;
 }
 
-string uppercase_string(const string& str) {
-    string s2(str.size(), '.');
+std::string uppercase_string(const std::string& str) {
+    std::string s2(str.size(), '.');
     transform(str.begin(), str.end(), s2.begin(), ::toupper);
     return s2;
 }
 
-string lowercase_string(const string& str) {
-    string s2(str.size(), '.');
+std::string lowercase_string(const std::string& str) {
+    std::string s2(str.size(), '.');
     transform(str.begin(), str.end(), s2.begin(), ::tolower);
     return s2;
 }
